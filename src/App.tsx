041a3e0c897c686fb89fb25c6faff33862cf64d0
nhimc9x <nhicomputer77@gmail.com>
2024-05-07 23:07:59 +0700

    Đặt Font chữ, màu sắc chính

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,16 @@
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="text-3xl bg-ct-primary text-ct-text-color h-96 px-10">
+      <div className="">
+        We are extending the default Tailwind configuration to expose a new font-poppins font family utility class. This means that it will exist alongside the Tailwind default font classes.
+      </div>
+      <div className="size-20 bg-ct-secondary"></div>
+      <div className="size-20 bg-ct-highlight-color"></div>
+      <div className="h-40 w-full bg-ct-secondary p-10">
+        We are extending the default Tailwind configuration to expose a new font-poppins font family utility class. This means that it will exist alongside the Tailwind default font classes.
+      </div>
+    </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ export default {
     './src/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        Niramit: ['"Niramit"', 'sans-serif']
+      },
+      screens: {
+        'xss': '375px',
+        'xs': '475px',
+        'sms': '575px',
+        'mdl': '960px'
+      },
+      colors: {
+        'ct-primary': '#0f0f0f',
+        'ct-secondary': '#272727',
+        'ct-highlight-color': '#ffd233',
+        'ct-text-color': '#a3a1be'
+      }
+    }
   },
   plugins: []
 }
